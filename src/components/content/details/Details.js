cefb96f038880729b89c7f138bc2b425d15c470a
Ryan <rcarrollct@gmail.com>
2021-01-14 16:34:29 -0500

    added details api service methods and actions

--- src/components/content/details/Details.js
+++ src/components/content/details/Details.js
@@ -1,61 +1,95 @@
-import React from 'react';
-import Rating from '../rating/Rating';
-import Crew from './crew/Crew';
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import './Details.scss';
-import Media from './media/Media';
-import Overview from './overview/Overview';
-import Reviews from './reviews/Reviews';
+import Rating from '../rating/Rating';
 import Tabs from './tabs/Tabs';
+import Overview from './overview/Overview';
+import Media from './media/Media';
+import Crew from './crew/Crew';
+import Reviews from './reviews/Reviews';
+import { movieDetails } from '../../../redux/actions/movies';
+import { IMAGE_URL } from '../../../services/movies.service';
 
-const Details = () => {
+const Details = (props) => {
+  const { movieDetails, movie } = props;
+  const [details, setDetails] = useState();
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (movie.length === 0) {
+      movieDetails(id);
+    }
+    setDetails(movie[0]);
+    // eslint-disable-next-line
+  }, [id, movie]);
+
   return (
     <>
-      <div className="movie-container">
-        <div className="movie-bg" style={{ backgroundImage: 'url(https://www.geeklawblog.com/wp-content/uploads/sites/528/2018/12/liprofile-656x369.png)' }}></div>
-        <div className="movie-overlay"></div>
-        <div className="movie-details">
-          <div className="movie-image">
-            <img src="https://www.geeklawblog.com/wp-content/uploads/sites/528/2018/12/liprofile-656x369.png" alt="" />
-          </div>
-          <div className="movie-body">
-            <div className="movie-overview">
-              <div className="movie-title">
-                Avengers <span>2020-12-03</span>
+      {
+        details &&
+          <div className="movie-container">
+            <div className="movie-bg" style={{ backgroundImage: `url(${IMAGE_URL}${details.backdrop_path})` }}></div>
+            <div className="movie-overlay"></div>
+            <div className="movie-details">
+              <div className="movie-image">
+                <img src={`${IMAGE_URL}${details.poster_path}`} alt="" />
               </div>
-              <div className="movie-genres">
-                <ul className="genres">
-                  <li>Action</li>
-                  <li>Comedy</li>
-                  <li>Sci-Fi</li>
-                </ul>
+              <div className="movie-body">
+                <div className="movie-overview">
+                  <div className="movie-title">
+                    {details.title} <span>{details.release_date}</span>
+                  </div>
+                  <div className="movie-genres">
+                    <ul className="genres">
+                      {
+                        details.genres.map((genre) => 
+                          <li key={genre.id}>{genre.name}</li>
+                        )
+                      }
+                    </ul>
+                  </div>
+                  <div className="movie-ratings">
+                    <Rating className="rating-stars" rating={details.vote_average} totalStars={10} />
+                    &nbsp;
+                    <span>{details.vote_average}</span> <p>({details.vote_count}) reviews</p>
+                  </div>
+                  <Tabs>
+                    <div label="Overview">
+                      <Overview />
+                    </div>
+                    <div label="Crew">
+                      <Crew />
+                    </div>
+                    <div label="Media">
+                      <Media />
+                    </div>
+                    <div label="Reviews">
+                      <Reviews />
+                    </div>
+                  </Tabs>
+                </div>
               </div>
-              <div className="movie-ratings">
-                <Rating className="rating-stars" rating={6.5} totalStars={10} />
-                &nbsp;
-                <span>6.5</span> <p>(200) reviews</p>
-              </div>
-              <Tabs>
-                <div label="Overview">
-                  <Overview />
-                </div>
-                <div label="Crew">
-                  <Crew />
-                </div>
-                <div label="Media">
-                  <Media />
-                </div>
-                <div label="Reviews">
-                  <Reviews />
-                </div>
-              </Tabs>
             </div>
           </div>
-        </div>
-        <div className></div>
-      </div>
+      }
+
     </>
   );
 };
 
-export default Details;
+Details.propTypes = {
+  movie: PropTypes.array,
+  movieDetails: PropTypes.func
+};
+
+const mapStateToProps = (state) => ({
+  movie: state.movies.movie
+});
+
+export default connect(
+  mapStateToProps,
+  { movieDetails }
+)(Details);
